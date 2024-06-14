e0502610feeb4800798c26ce5d2760e66f439c84
XtraDavido <davidokemink8@gmail.com>
2024-06-14 09:16:47 +0200

    Mod Setup

---
+++ src/main/java/net/xtrajohn/mmi/MinecraftModInformaticaClient.java
@@ -0,0 +1,10 @@
+package net.xtrajohn.mmi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftModInformaticaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
